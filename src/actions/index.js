6cc5e9d01b809842e603aac1aeb966299bb1511d
Allan Carlos <allancarlosdeveloper@gmail.com>
2018-02-24 10:45:37 -0300

    first router

---
+++ src/actions/index.js
@@ -0,0 +1,4 @@
+import axios from 'axios';
+
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+const API_KEY = '?key=PAPERCLIP1234';
--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+import reducers from './reducers';
+import PostsIndex from './components/PostsIndex';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <BrowserRouter>
+            <div>
+                <Route path='/' component={PostsIndex} />
+            </div>
+        </BrowserRouter>
+    </Provider>,
+    document.getElementById('root')
+);
