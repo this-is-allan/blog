a31d60ecb72d1aa334746413d28cfb27a9f77cac
Allan Carlos <allancarlosdeveloper@gmail.com>
2018-02-24 14:57:36 -0300

    Delete a post

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,10 +1,10 @@
 import _ from "lodash";
-import { FETCH_POSTS, FETCH_POST } from "../actions";
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from "../actions";
 
 export default function (state = {}, action) {
     switch (action.type) {
         case FETCH_POST:
-            return { ...state, [action.payload.data.id]: action.payload.data };
+            return _.omit(state, action.payload);
         case FETCH_POSTS:
             return _.mapKeys(action.payload.data, "id");
         default:
